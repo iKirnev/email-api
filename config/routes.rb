236b3f180eb26437b48b431947bfbec0d8453365
Ivan <ivan.kirnev@gmail.com>
2018-12-01 16:41:46 +0200

    interface improvements

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'api/pipl'
   get 'api/fullcontact'
   post 'api/fullcontact'
-  get 'welcome/index'
-  root 'welcome#index'
+  root 'api#pipl'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
